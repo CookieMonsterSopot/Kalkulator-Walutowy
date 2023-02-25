5590e5428dc94bd697ffc7b85311475394247cac
Tomasz Sz <tomasz.szawara@gmail.com>
2023-02-25 11:39:41 +0100

    podpowiedzi na dalsza optymalizacje

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import { useState, useEffect } from "react";
 import CurrencySelector from "./CurrencySelector";
 
+// Porada: axios.get(`http.....') kod jest powtarzany, mozna stworzyc funkcje (zadeklarowana przed funkcja komponentu, ktora obsluzy to )
+// - najkorzystniej byloby aby mozna bylo odpalic getCurrencyData(currencyName: string)
+
 const Currency = () => {
   const [inSelected, setInSelected] = useState("chf");
   const [outSelected, setOutSelected] = useState("usd");
 
   const [startAmount, setStartAmuount] = useState("0");
 
-  const [inCurrency, setInCurrency] = useState(0);
+  const [inCurrency, setInCurrency] = useState(0); // change name to firstCurrencyValueInPLN
   useEffect(() => {
     axios
       .get(`http://api.nbp.pl/api/exchangerates/rates/a/${inSelected}/?`)
@@ -18,7 +21,7 @@ const Currency = () => {
       );
   }, [inSelected]);
 
-  const [outCurrency, setOutCurrency] = useState(0);
+  const [outCurrency, setOutCurrency] = useState(0); // change name to secondCurrencyValueInPLN
   useEffect(() => {
     axios
       .get(`http://api.nbp.pl/api/exchangerates/rates/a/${outSelected}/?`)
